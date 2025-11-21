68bc0d5c534e5b79cbddce3492a4cc36a233172a
NessieflakeS <nikandrof.danil@gmail.com>
2025-11-21 18:16:49 +0800

    .

--- src/components/Analytics/Calendar.jsx
+++ src/components/Analytics/Calendar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useApp } from '../../context/AppContext';
-import './Calendar.css';
+import './AnalyticsChart.css';
 
 const Calendar = () => {
   const { analyticsPeriod, dispatch } = useApp();
